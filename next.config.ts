1a6718dac7f7fa4cd262790fc2c489f93c82285d
LsChavve <lchavvase@gmail.com>
2026-01-17 12:03:59 +0700

    chore: force redeploy for edge runtime fixes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Fresh build trigger
+  // Build trigger: Edge Runtime Update
 };
 
 export default nextConfig;
